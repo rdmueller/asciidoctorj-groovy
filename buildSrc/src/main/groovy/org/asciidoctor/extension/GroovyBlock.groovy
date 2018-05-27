c8e5e28a6ff3a52a9778fb4dad216d44da74c287
Jorge <jagedn@gmail.com>
2018-05-27 21:24:59 +0200

    Build two Blocks
    I was playing with it and I found it's not necessary to return a block with two children. We can add them to the parent and return null if we want to maintain they separate
    
    What I was not able is to create blocks with title or apply a source style

--- buildSrc/src/main/groovy/org/asciidoctor/extension/GroovyBlock.groovy
+++ buildSrc/src/main/groovy/org/asciidoctor/extension/GroovyBlock.groovy
@@ -9,7 +9,7 @@ class GroovyBlock extends BlockProcessor {
     private static binding = new Binding()
 
     GroovyBlock(String name, Map<String, Object> config) {
-        super(name, [contexts: [':paragraph'], content_model: ':simple'])
+        super(name, [contexts: [':paragraph'], content_model: ':verbatim'])
         binding.context = [:]
     }
 
@@ -17,19 +17,17 @@ class GroovyBlock extends BlockProcessor {
         def code = reader.lines().join("\n")
         def shell = new GroovyShell(binding)
         def result = shell.evaluate(code)
-        def block="""
-<div class="listingblock">
-    <div class="content">
-        <pre class="CodeRay highlight"><code class="language-groovy" data-lang="groovy">$code</code></pre>
-    </div>
-    <div class="content" style="margin-top: 3px;">
-        <pre class="highlight"><code data-lang="output">$result</code></pre>
-    </div>
-</div>
-"""
+
         //second parameter is "context" which seems to take one of
         //these values: https://github.com/asciidoctor/asciidoctor/blob/master/lib/asciidoctor/block.rb#L14
-        createBlock(parent, 'pass', block, attributes, [:])
+
+        def codeblock = createBlock(parent, 'listing', "$code", [:],[:])
+        parent.blocks().add(codeblock)
+
+        def resultBlock = createBlock(parent, 'listing', "$result", [:], [:])
+        parent.blocks().add(resultBlock)
+
+        null
     }
 
 
